b0fc29692ac74fbb62fc8fc12c31c0ab051fb636
Dave Charm Bulaquena <i_davecharm.bulaquena@stratpoint.com>
2024-05-15 22:45:53 +0800

    feat(teailwind): Added tailwind setup to thios project

--- src/pages/home/dashboardPage/dashboardPage.jsx
+++ src/pages/home/dashboardPage/dashboardPage.jsx
@@ -1,6 +1,6 @@
 
 export default function DashboardPage() {
   return (
-    <div style={{width: "1000px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem veritatis ullam officia tempore vitae incidunt esse voluptates deleniti aspernatur doloribus sunt molestiae numquam illum, hic dolor dolorum cumque facere sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. In, delectus harum iste, autem consequuntur consequatur iure incidunt exercitationem molestiae provident non architecto sit esse cupiditate tempora vitae quod adipisci veniam.</div>
+    <div style={{ width: "1000px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem veritatis ullam officia tempore vitae incidunt esse voluptates deleniti aspernatur doloribus sunt molestiae numquam illum, hic dolor dolorum cumque facere sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. In, delectus harum iste, autem consequuntur consequatur iure incidunt exercitationem molestiae provident non architecto sit esse cupiditate tempora vitae quod adipisci veniam.</div>
   )
 }
