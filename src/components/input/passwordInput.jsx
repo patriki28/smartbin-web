7ac352086de5456364e887b3d2f86e6ab14d3971
patriki28 <diestapatrick1@gmail.com>
2024-05-25 13:59:53 +0800

    fix: add required fields

--- src/components/input/passwordInput.jsx
+++ src/components/input/passwordInput.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { BsEye, BsEyeSlash } from 'react-icons/bs';
 
-export default function PasswordInput({ password, onChange, placeholder }) {
+export default function PasswordInput({ name, password, onChange, placeholder }) {
     const [showPassword, setShowPassword] = useState(false);
 
     return (
         <div className="form-control d-flex justify-content-between align-items-center">
             <input
-                placeholder={placeholder ? placeholder : 'Enter Password'}
+                name={name}
                 value={password}
-                className="w-100 border-0 fs-5 outline-0"
-                type={showPassword ? 'text' : 'password'}
                 onChange={onChange}
+                type={showPassword ? 'text' : 'password'}
+                className="w-100 border-0 fs-5 outline-0"
+                placeholder={placeholder ? placeholder : 'Enter Password'}
+                required
             />
             <Button variant="outline-secondary" className="border-0" onClick={() => setShowPassword(!showPassword)}>
                 {showPassword ? <BsEyeSlash size={30} /> : <BsEye size={30} />}
